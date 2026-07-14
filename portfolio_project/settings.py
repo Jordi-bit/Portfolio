76c2526106eb3d74747c1832d393181625189d17
jordi <bitcurso3@gmail.com>
2026-07-14 10:43:34 +0200

    adaptacion a moviles

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,9 +25,11 @@ SECRET_KEY = 'django-insecure-m8&rp2hicx#s2$!es!)^g$9@@-wnxedytu_1_$&030-3h-@d(#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['profile.4004c.com', 'localhost', '127.0.0.1']
 
-
+CSRF_THUSTED_ORIGINS = [
+    "https://profile.4004c.com",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
